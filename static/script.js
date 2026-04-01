7deecadd24e0cee5adededd61bdc175c4328b466
zversi2 <z.versi22@gmail.com>
2026-03-31 20:44:42 -0400

    Modify homepage index and style

--- static/script.js
+++ static/script.js
@@ -1,3 +1,8 @@
+let responseFlow = {
+    fullText: "",
+    step: null
+};
+
 async function sendMessage(customMessage = null) {
     const inputField = document.getElementById("userInput");
 
@@ -32,9 +37,16 @@ async function sendMessage(customMessage = null) {
 
     document.getElementById("typing").remove();
 
+    // Save full response
+    responseFlow.fullText = data.response;
+    responseFlow.step = "definition";
+
+    // Take only first 1–2 sentences
+    let shortResponse = data.response.split(". ").slice(0, 2).join(". ") + ".";
+
     chatbox.innerHTML += `
         <div class="bot">
-            <div class="bubble">${data.response}</div>
+            <div class="bubble">${shortResponse} Want to learn more? (yes/no)</div>
         </div>
     `;
 
